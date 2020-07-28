00f8108679001cf9e4bede489d6f7970c2a0b151
keitolee <keitolee96@gmail.com>
2020-07-27 23:19:51 -0700

    created reverse.cpp containing main

---
+++ reversi.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "reversi.h"
+
+int main(){
+    char board[3][4] = { {' ', ' ', ' ', '\0'}, {' ', ' ', ' ', '\0'}, {' ', ' ', ' ', '\0'} };
+
+    printBoard(board);
+}
