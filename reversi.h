1aaa2edea665130b9b67fd8ee802c763767424a2
keitolee <keitolee96@gmail.com>
2020-08-07 12:31:17 -0700

    added check_position function to check position already has a piece

--- reversi.h
+++ reversi.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// print the current state of the game board
 void printBoard(char board[8][9]){
     for (int i = 0; i < 72; i++)
     {
@@ -22,3 +23,17 @@ void printBoard(char board[8][9]){
         }
     }
 }
+
+// checks if input place has a piece already at position.
+// return 1 for position availible, 0 for position already taken 
+int check_position(char board[8][9], int x, int y){
+
+    if (board[y][x] == ' '){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+}
+
