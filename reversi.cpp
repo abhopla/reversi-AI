4bc8f73938de2b9764282f4a82f6766907d32359
abhopla <bhoplaanubhav@gmail.com>
2020-08-07 22:58:02 -0700

    Finished the first basic version of play logic

--- reversi.cpp
+++ reversi.cpp
@@ -1,20 +1,27 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "reversi.h"
 
 int main(){
-    char board[8][9] = { {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'}, 
+    char board_2[8][9] = { {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'}, 
                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
-                        {' ', ' ', ' ', 'W', 'B', ' ', ' ', ' ', '\0'},
-                        {' ', ' ', ' ', 'B', 'W', ' ', ' ', ' ', '\0'},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'},
                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\0'}, };
 
-    printBoard(board);
+    
+    board_2[1][1] = 'G';
 
-    int moves = legal_moves(board, 'G');
+    printBoard(board_2);
+
+    int moves = legal_moves(board_2, 'G');
     cout <<"moves: " << moves << endl;
+
+    play();
+    positionParse (35);
 }
