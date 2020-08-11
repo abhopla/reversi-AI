273cd398827f9a75e5b4671870e11d2786c0eeca
abhopla <bhoplaanubhav@gmail.com>
2020-08-10 22:25:39 -0700

    changed game_state() method so it recognizes the end of a game correctly

--- reversi.h
+++ reversi.h
@@ -106,48 +106,52 @@ class Reversi{
                                 if (board[i][k] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                             }
                         }
                         // position to the left is white
-                        else if (board[i][j-1] == 'W'){
+                        if (board[i][j-1] == 'W'){
                             // check along the left of the position and confirm that there is a black piece after consecutive white pieces
                             int k = j-1;
                             while(k >= 0){
                                 if (board[i][k] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                             }
                         }
                         // position below is white
-                        else if (board[i+1][j] == 'W'){
+                        if (board[i+1][j] == 'W'){
                             // check along below the position and confirm that there is a black piece after consecutive white pieces
                             int k = i+1;
                             while(k < 8){
                                 if (board[k][j] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                             }
                         }
                         // position above is white
-                        else if (board[i-1][j] == 'W'){
+                        if (board[i-1][j] == 'W'){
                             // check along above the position and confirm that there is a black piece after consecutive white pieces
                             int k = i-1;
                             while(k >= 0){
                                 if (board[k][j] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                             }
                         }
                         // position to the bottom right is white
-                        else if (board[i+1][j+1] == 'W'){
+                        if (board[i+1][j+1] == 'W'){
                             // check along the diagonal (below right) position and confirm that there is a black piece after consecutive white pieces
                             int k = i+1;
                             int r = j+1;
@@ -155,13 +159,14 @@ class Reversi{
                                 if (board[k][r] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                                 r++;
                             }
                         }
                         // position to the top right is white
-                        else if (board[i-1][j+1] == 'W'){
+                        if (board[i-1][j+1] == 'W'){
                             // check along the diagonal (above right) position and confirm that there is a black piece after consecutive white pieces
                             int k = i-1;
                             int r = j+1;
@@ -169,13 +174,14 @@ class Reversi{
                                 if (board[k][r] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                                 r++;
                             }
                         }
                         // position to the bottom left is white
-                        else if (board[i+1][j-1] == 'W'){
+                        if (board[i+1][j-1] == 'W'){
                             // check along the diagonal (above right) position and confirm that there is a black piece after consecutive white pieces
                             int k = i+1;
                             int r = j-1;
@@ -183,13 +189,14 @@ class Reversi{
                                 if (board[k][r] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                                 r--;
                             }
                         }
                         // position to the top left is white
-                        else if (board[i-1][j-1] == 'W'){
+                        if (board[i-1][j-1] == 'W'){
                             // check along the diagonal (below left) position and confirm that there is a black piece after consecutive white pieces
                             int k = i-1;
                             int r = j-1;
@@ -197,6 +204,7 @@ class Reversi{
                                 if (board[k][r] == 'B'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                                 r--;
@@ -213,48 +221,52 @@ class Reversi{
                                 if (board[i][k] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                             }
                         }
                         // position to the left is black
-                        else if (board[i][j-1] == 'B'){
+                        if (board[i][j-1] == 'B'){
                             // check along the left of the position and confirm that there is a white piece after consecutive white pieces
                             int k = j-1;
                             while(k >= 0){
                                 if (board[i][k] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                             }
                         }
                         // position below is black
-                        else if (board[i+1][j] == 'B'){
+                        if (board[i+1][j] == 'B'){
                             // check along below the position and confirm that there is a white piece after consecutive white pieces
                             int k = i+1;
                             while(k < 8){
                                 if (board[k][j] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                             }
                         }
                         // position above is black
-                        else if (board[i-1][j] == 'B'){
+                        if (board[i-1][j] == 'B'){
                             // check along above the position and confirm that there is a white piece after consecutive white pieces
                             int k = i-1;
                             while(k >= 0){
                                 if (board[k][j] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                             }
                         }
                         // position to the bottom right is black
-                        else if (board[i+1][j+1] == 'B'){
+                        if (board[i+1][j+1] == 'B'){
                             // check along the diagonal (below right) position and confirm that there is a white piece after consecutive white pieces
                             int k = i+1;
                             int r = j+1;
@@ -262,13 +274,14 @@ class Reversi{
                                 if (board[k][r] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                                 r++;
                             }
                         }
                         // position to the top right is black
-                        else if (board[i-1][j+1] == 'B'){
+                        if (board[i-1][j+1] == 'B'){
                             // check along the diagonal (above right) position and confirm that there is a white piece after consecutive white pieces
                             int k = i-1;
                             int r = j+1;
@@ -276,13 +289,14 @@ class Reversi{
                                 if (board[k][r] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                                 r++;
                             }
                         }
                         // position to the bottom left is black
-                        else if (board[i+1][j-1] == 'B'){
+                        if (board[i+1][j-1] == 'B'){
                             // check along the diagonal (above right) position and confirm that there is a white piece after consecutive white pieces
                             int k = i+1;
                             int r = j-1;
@@ -290,13 +304,14 @@ class Reversi{
                                 if (board[k][r] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k++;
                                 r--;
                             }
                         }
                         // position to the top left is black
-                        else if (board[i-1][j-1] == 'B'){
+                        if (board[i-1][j-1] == 'B'){
                             // check along the diagonal (below left) position and confirm that there is a white piece after consecutive white pieces
                             int k = i-1;
                             int r = j-1;
@@ -304,6 +319,7 @@ class Reversi{
                                 if (board[k][r] == 'W'){
                                     moves_list[count] = coordinates(i, j);
                                     count++;
+                                    goto afterLoop;
                                 }
                                 k--;
                                 r--;
@@ -311,6 +327,9 @@ class Reversi{
                         }
                     }
                 }
+                // end of loop for each position
+                afterLoop:;
+                
             }
         }
 
@@ -331,10 +350,42 @@ class Reversi{
     // if black win returns 1
     // if white win returns 2
     // if tie return 3
-    int game_state(){
+    int game_state(char color){
 
         int blk = 0;
         int whi = 0;
+
+        legal_moves(color);
+
+
+        // Check is the number of valid moves is 
+        // 0 
+        if(num_of_valid_moves == 0){
+            for (int i=0; i<8; i++){
+                for (int j=0; j<8; j++){
+
+                    if (board[i][j] == 'B'){
+                        blk++;
+                    }
+                    else if (board[i][j] == 'W'){
+                        whi++;
+                    }
+                }
+            }
+
+            if(blk > whi){
+              return 1;
+
+            }
+            else if(blk < whi){
+              return 2;
+            }
+            else if(blk == whi){
+              return 3;
+            }
+
+          
+        }
 
         // go through every position, if blank found return 0, otherwise count white and black tiles
         for (int i=0; i<8; i++){
@@ -634,6 +685,38 @@ class Reversi{
         }
     }
 
+
+    // Checks what type of AI the user wants to play against
+    int ai_choice(){
+      int choice;
+      cout << "Please choose whether you would like to play against the Pure MCTS (1) or the heuritic based MCTS (2) (1/2):  ";
+      int check = 0;
+
+      // This while loop is checking for invalid types 
+      while(check == 0){
+        while(!(cin >> choice)){
+          cout << "Invalid type of argument! Please enter an integer to proceed: ";
+          cin.clear();
+          cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        if((choice == 1) || (choice == 2)){
+          check = 1;
+          break;
+        }
+
+        cout << "Invalid input please enter (1/2): ";
+
+
+      }
+
+
+      return choice;      
+
+
+
+    }
+
     // Checks whether the player wants to go first or second 
     // and validates the input 
     // sends back their answer 
@@ -668,13 +751,17 @@ class Reversi{
 
     // Purpose of this function is to validate 
     // player moves 
-    // it needs the move and the color of their chips  
+    // it needs the color of their chips  
     int valid_player_choice(int color){
       int * legal_move_arr = legal_moves(color);
       int count = 0;
       int move = 0;
       int num_check = 0;
       int toggle = 0;
+
+      // if(num_of_valid_moves == 0){
+      //   return 0;
+      // }
 
 
       cout << "These are the legal moves you are allowed to make: ";
@@ -728,52 +815,74 @@ class Reversi{
 
     void play (){
 
-    int choice = validate_first_or_second();
-   
-        if(choice == 1){
-            cout << "You are going first! " << endl;
-            int player_choice_1;
-            int player_choice_2;
-            int available_moves_1[64];
-            int move_count = 0;
-            int state_check;
+        int choice = validate_first_or_second();
+        int ai_type = ai_choice();
+    
+            if(choice == 1){
+                cout << "You are going first! " << endl;
+                int player_choice_1;
+                int player_choice_2;
+                int state_check = 0;
 
-            while((num_of_valid_moves != 0) || (state_check == 0)){
+                while (state_check == 0){
+                
 
-              // First player goes 
-              player_choice_1 = valid_player_choice('b');
-              make_move(player_choice_1, 'b');
+                state_check = game_state('b');
 
-              cout << "This is the current board state: "<<endl;
-              printBoard();
+                if(state_check != 0){
+                  break;
+                }
+                // First player goes 
+                player_choice_1 = valid_player_choice('b');
+                make_move(player_choice_1, 'b');
 
-              //Second player goes
-               player_choice_2 = valid_player_choice('w');
-               make_move(player_choice_2, 'w');
+                cout << "This is the current board state: "<<endl;
+                printBoard();
 
-              cout << "This is the current board state: "<<endl;
-              printBoard();
-               
-               state_check = game_state();
+                // if(ai_type == 1){
+                //   cout << "you are playing against the pure MCTS"<<endl;
+                // } else if(ai_type == 2){
+                //   cout << "you are playing against the heuristic based MCTS"<<endl;
+                // }
+
+                state_check = game_state('w');
+                if(state_check != 0){
+                  break;
+                }
+                //Second player goes
+                player_choice_2 = valid_player_choice('w');
+                make_move(player_choice_2, 'w');
+
+
+                cout << "This is the current board state: "<<endl;
+                printBoard();
+
+                
+
+                }
+
+                if(state_check == 1){
+                  cout << "Black chip wins! " <<endl;
+                } 
+                else if(state_check == 2){
+                  cout << "White chip wins! " <<endl;
+                }
+                else if(state_check == 3){
+                  cout << "The game is a tie";
+                }
+
+                
+            }
+
+            if(choice == 2){
+            cout << "You are going second! " << endl;
 
             }
 
-            
-
-            
         }
-
-        if(choice == 2){
-        cout << "You are going second! " << endl;
-
-        }
-
-    }
 
 
 };
-
-
 
 
 
