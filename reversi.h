c588a5b689f6f1625fd18928cc73c9436734bef6
keitolee <keitolee96@gmail.com>
2020-08-11 00:40:06 -0700

    playouts will now total wins lossses and ties of simulated games

--- reversi.h
+++ reversi.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <time.h>
 
 using namespace std;
 
@@ -30,6 +31,8 @@ class Reversi{
 
     int num_of_valid_moves = 0;
 
+    int playouts_valid_moves = 0;
+
 
 
     // print the current state of the game board
@@ -49,6 +52,7 @@ class Reversi{
     }
 
 
+
     // checks if input place has a piece already at position.
     // return 1 for position availible, 0 for position already taken 
     int check_position(int x, int y){
@@ -61,6 +65,7 @@ class Reversi{
         }
 
     }
+
 
 
     int * positionParse (int pos){
@@ -77,6 +82,7 @@ class Reversi{
     }
 
 
+
     // Parse the coordinates into a position 
     int coordinates(int y, int x){
         int temp_y = y + 1;
@@ -85,6 +91,8 @@ class Reversi{
         return pos;
 
     }
+
+
 
     // input current game state and player and return array of all legal moves
     int *legal_moves(char player){
@@ -343,7 +351,7 @@ class Reversi{
         }
 
         num_of_valid_moves = count;
-        // cout << "this is the number of moves " << num_of_valid_moves << endl;
+        // cout << "reg: this is the number of moves " << num_of_valid_moves << endl;
         static int *valid_moves = new int[count];
 
         for (int i=0; i<count; i++){
@@ -374,7 +382,7 @@ class Reversi{
             // j going left to right cols
             for (int j=0; j<8; j++){
                 // check for empty position
-                if (check_position(j, i) == 1){
+                if (playout_board[i][j] == ' '){
                     // if player color is black
                     if (player == 'b'){
                         // position to the right is white
@@ -612,29 +620,61 @@ class Reversi{
             }
         }
 
-        num_of_valid_moves = count;
-        // cout << "this is the number of moves " << num_of_valid_moves << endl;
-        static int *valid_moves = new int[count];
+        playouts_valid_moves = count;
+        // cout << "playouts: this is the number of moves " << playouts_valid_moves << endl;
+        static int *curr_moves = new int[count];
 
         for (int i=0; i<count; i++){
-            valid_moves[i] = moves_list[i];
+            curr_moves[i] = moves_list[i];
         }
 
-        return valid_moves;
+        return curr_moves;
     }
 
 
 
 
-    // check to see if game is complete
+    // check to see if simulated game is complete
     // if game incomplete return 0 
     // if black win returns 1
     // if white win returns 2
     // if tie return 3
-    int game_state(){
+    int game_state(char color){
 
         int blk = 0;
         int whi = 0;
+
+        legal_moves(color);
+
+
+        // Check is the number of valid moves is 
+        // 0 
+        if(num_of_valid_moves == 0){
+            for (int i=0; i<8; i++){
+                for (int j=0; j<8; j++){
+
+                    if (board[i][j] == 'B'){
+                        blk++;
+                    }
+                    else if (board[i][j] == 'W'){
+                        whi++;
+                    }
+                }
+            }
+
+            if(blk > whi){
+              return 1;
+
+            }
+            else if(blk < whi){
+              return 2;
+            }
+            else if(blk == whi){
+              return 3;
+            }
+
+          
+        }
 
         // go through every position, if blank found return 0, otherwise count white and black tiles
         for (int i=0; i<8; i++){
@@ -668,33 +708,25 @@ class Reversi{
 
 
 
-    // check to see if simulated game is complete
-    // if game incomplete return 0 
-    // if black win returns 1
-    // if white win returns 2
-    // if tie return 3
-<<<<<<< HEAD
-    int playout_game_state(char playout_board[8][9]){
-=======
-    int game_state(char color){
->>>>>>> 1889bfcb5a58ddec20282858dbbb8ae0075d1d00
+    // checking game state for simulated playouts
+    int playouts_game_state(char color, char playout_board[8][9]){
 
         int blk = 0;
         int whi = 0;
 
-        legal_moves(color);
+        playouts_legal_moves(color, playout_board);
 
 
         // Check is the number of valid moves is 
         // 0 
-        if(num_of_valid_moves == 0){
+        if(playouts_valid_moves == 0){
             for (int i=0; i<8; i++){
                 for (int j=0; j<8; j++){
 
-                    if (board[i][j] == 'B'){
+                    if (playout_board[i][j] == 'B'){
                         blk++;
                     }
-                    else if (board[i][j] == 'W'){
+                    else if (playout_board[i][j] == 'W'){
                         whi++;
                     }
                 }
@@ -1013,7 +1045,6 @@ class Reversi{
     }
 
 
-<<<<<<< HEAD
 
     // make_move but for random playouts on copy_board
     void playouts_make_move(int position, char color, char playout_board[8][9]){
@@ -1283,7 +1314,6 @@ class Reversi{
 
 
 
-=======
     // Checks what type of AI the user wants to play against
     int ai_choice(){
       int choice;
@@ -1305,17 +1335,15 @@ class Reversi{
 
         cout << "Invalid input please enter (1/2): ";
 
-
       }
-
 
       return choice;      
 
-
-
     }
 
->>>>>>> 1889bfcb5a58ddec20282858dbbb8ae0075d1d00
+
+
+
     // Checks whether the player wants to go first or second 
     // and validates the input 
     // sends back their answer 
@@ -1406,44 +1434,85 @@ class Reversi{
     void playouts(char color){
         
         int *moves = legal_moves(color);
-        int playout_num = 100;
 
         // loop through each of the current valid moves
         for (int i=0; i<num_of_valid_moves; i++){
 
-            // make copy_board reflect the curent state of the board
-            for (int i=0; i<8; i++){
-                for (int j=0; j<9; j++){
-                    copy_board[i][j] = board[i][j];
+            int wins = 0;
+            int ties = 0;
+            int losses = 0;
+
+            for (int k=0; k<1000; k++){
+
+                // make copy_board reflect the curent state of the board
+                for (int i=0; i<8; i++){
+                    for (int j=0; j<9; j++){
+                        copy_board[i][j] = board[i][j];
+                    }
                 }
-            }
 
-            playouts_make_move(moves[i], color, copy_board);
+                // make move of the current valid move
+                playouts_make_move(moves[i], color, copy_board);
 
-            // // make a copy of the curent state of the board
-            // char new_board[8][9];
-            // for (int i=0; i<8; i++){
-            //     for (int j=0; j<9; j++){
-            //         new_board[i][j] = board[i][j];
-            //     }
-            // }
-
-            char curr_player_col = color;
-
-            // game is not over
-            while(){
-
-
+                // switch player color
+                char curr_player_col = color;
                 if (curr_player_col == 'b'){
-                    curr_player_col = 'w';
-                }
+                        curr_player_col = 'w';
+                    }
                 else if ( curr_player_col == 'w'){
-                    curr_player_col = 'b'
+                    curr_player_col = 'b';
                 }
+
+
+                // while game is not over
+                while(playouts_game_state(curr_player_col, copy_board) == 0){
+
+                    // get all legal moves 
+                    int *move_options = playouts_legal_moves(curr_player_col, copy_board);
+
+                    // play random position
+                    int RandIndex = rand() % playouts_valid_moves;
+                    playouts_make_move(move_options[RandIndex], curr_player_col, copy_board);
+
+                    // switch colors for next move
+                    if (curr_player_col == 'b'){
+                        curr_player_col = 'w';
+                    }
+                    else if ( curr_player_col == 'w'){
+                        curr_player_col = 'b';
+                    }
+
+                }
+
+                if (playouts_game_state(curr_player_col, copy_board) == 1){
+                    if (color == 'b'){
+                        wins++;
+                    }
+                    else if (color == 'w'){
+                        losses++;
+                    }
+                }
+                else if (playouts_game_state(curr_player_col, copy_board) == 2){
+                    if (color == 'w'){
+                        wins++;
+                    }
+                    else if (color == 'b'){
+                        losses++;
+                    }
+                }
+                else if (playouts_game_state(curr_player_col, copy_board) == 3){
+                    ties++;
+                }
+
+                
+
             }
 
-            
-
+            cout << "Record for " << moves[i] << endl;
+            cout << "wins: " << wins << endl;
+            cout << "losses: " << losses << endl;
+            cout << "ties: " << ties << endl;
+            cout << endl;
 
         }
 
@@ -1464,63 +1533,42 @@ class Reversi{
                 cout << "You are going first! " << endl;
                 int player_choice_1;
                 int player_choice_2;
-<<<<<<< HEAD
-                int move_count = 0;
-=======
->>>>>>> 1889bfcb5a58ddec20282858dbbb8ae0075d1d00
                 int state_check = 0;
 
                 while (state_check == 0){
                 
 
-<<<<<<< HEAD
+                    state_check = game_state('b');
+
+                    if(state_check != 0){
+                        break;
+                    }
                     // First player goes 
                     player_choice_1 = valid_player_choice('b');
                     make_move(player_choice_1, 'b');
-=======
-                state_check = game_state('b');
 
-                if(state_check != 0){
-                  break;
-                }
-                // First player goes 
-                player_choice_1 = valid_player_choice('b');
-                make_move(player_choice_1, 'b');
->>>>>>> 1889bfcb5a58ddec20282858dbbb8ae0075d1d00
+                        cout << "This is the current board state: "<<endl;
+                        printBoard();
 
-                    cout << "This is the current board state: "<<endl;
-                    printBoard();
+                    // if(ai_type == 1){
+                    //   cout << "you are playing against the pure MCTS"<<endl;
+                    // } else if(ai_type == 2){
+                    //   cout << "you are playing against the heuristic based MCTS"<<endl;
+                    // }
 
-<<<<<<< HEAD
+                    state_check = game_state('w');
+                    if(state_check != 0){
+                        break;
+                    }
                     //Second player goes
                     player_choice_2 = valid_player_choice('w');
                     make_move(player_choice_2, 'w');
 
+
                     cout << "This is the current board state: "<<endl;
                     printBoard();
-                    
-                    state_check = game_state();
-=======
-                // if(ai_type == 1){
-                //   cout << "you are playing against the pure MCTS"<<endl;
-                // } else if(ai_type == 2){
-                //   cout << "you are playing against the heuristic based MCTS"<<endl;
-                // }
-
-                state_check = game_state('w');
-                if(state_check != 0){
-                  break;
-                }
-                //Second player goes
-                player_choice_2 = valid_player_choice('w');
-                make_move(player_choice_2, 'w');
-
-
-                cout << "This is the current board state: "<<endl;
-                printBoard();
 
                 
->>>>>>> 1889bfcb5a58ddec20282858dbbb8ae0075d1d00
 
                 }
 
@@ -1538,7 +1586,7 @@ class Reversi{
             }
 
             if(choice == 2){
-            cout << "You are going second! " << endl;
+                cout << "You are going second! " << endl;
 
             }
 
